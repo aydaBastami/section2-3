6fe8c88e229caac4720f9cb5622e134ff47cd17d
aydaBastami <aydabstmi@gmail.com>
2021-05-18 11:42:10 -0700

    ida2

---
+++ script.js
@@ -0,0 +1,31 @@
+// let newH1 = document.createElement("h1")
+// document.body.append(newH1)
+// newH1.innerText = "Welcome"
+
+// function makeColor () {
+//     let r = Math.floor(Math.random() * 255)
+//     let g = Math.floor(Math.random() * 255)
+//     let b = Math.floor(Math.random() * 255)
+//     return `rgb(${r}, ${g}, ${b})`
+// }
+
+// let style = newH1.addEventListener("click", () => {
+//     document.body.style.backgroundColor = makeColor()
+//     newH1.style.color = makeColor()
+// })
+
+// let input = document.querySelector("input");
+// input.addEventListener("keydown", (e) => {
+// //   console.log(e.code);
+//   switch (e.code) {
+//       case "ArrowUp":
+//           console.log("UP!");
+//           break;
+//         case "ArrowDown":
+//             console.log("Down!");
+//             break;
+//       default:
+//           console.log(e.code);
+//   }
+// });
+
